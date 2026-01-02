6f25bc7da33de91a599364b172b2500c9442524f
Livewireprod <technical@livewireproductions.co.uk>
2026-01-02 15:52:01 +0000

    Update endpoints.js

--- apps/ui/src/config/endpoints.js
+++ apps/ui/src/config/endpoints.js
@@ -3,3 +3,5 @@ const isHttps = location.protocol === "https:";
 export const GATEWAY_HTTP = ""; // same origin
 
 export const GATEWAY_WS = `${isHttps ? "wss" : "ws"}://${location.host}/events`;
+
+// https://github.com/Livewireprod/bambu-site
